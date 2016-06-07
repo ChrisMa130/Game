fa8fc7fde327e463d73cea2747cb363e85a56255
jlu3389 <jlu3389@gmail.com>
2016-06-07 16:04:23 +0800

    漏提了。

---
+++ Product/Assets/GameScripts/ObjectLogic/autodestory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MG
+{
+    public class autodestory : MonoBehaviour
+    {
+        public float DestoryTime = 0;
+        void Update()
+        {
+            if (DestoryTime <= 0)
+            {
+                GameObject.Destroy(gameObject);
+            }
+
+            DestoryTime -= Time.unscaledDeltaTime;
+        }
+    }
+
+}
+
