3a744cff356d4840ad5850ab25dc22ddcbb70d82
jlu3389 <jlu3389@gmail.com>
2016-06-13 02:16:06 +0800

    = = who ignore the  root path.....

---
+++ Product/Assets/GameScripts/ObjectLogic/ForbiddenZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MG
+{
+    public class ForbiddenZone : MonoBehaviour
+    {
+        void OnMouseDown()
+        {
+            GameMgr.Instance.LineMgr.CanDraw = false;
+        }
+
+        void OnMouseOver()
+        {
+            if (GameMgr.Instance.LineMgr.CanDraw)
+                GameMgr.Instance.LineMgr.CanDraw = false;
+        }
+        void OnMouseExit()
+        {
+            GameMgr.Instance.LineMgr.CanDraw = true;
+        }
+    }
+
+}
+
+
