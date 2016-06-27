7b318b1341598f1371096e431aa7cc4fc693ee1f
jlu3389 <jlu3389@gmail.com>
2016-06-27 21:06:48 +0800

    楼梯实现

--- Product/Assets/GameScripts/NpcLogic/NpcSimpleAi.cs
+++ Product/Assets/GameScripts/NpcLogic/NpcSimpleAi.cs
@@ -6,10 +6,14 @@ namespace MG
     public class NpcSimpleAi : MonoBehaviour
     {
         private Npc NpcObject;
+        private float CurrentNpcClimbHeight;
 
         void Start()
         {
             NpcObject = gameObject.GetComponent<Npc>();
+
+            var c2d = NpcObject.GetComponent<BoxCollider2D>();
+            CurrentNpcClimbHeight = c2d.bounds.size.y/2 - GameDefine.StairsSlopeHeight;
         }
 
         void Update()
@@ -31,11 +35,22 @@ namespace MG
                 Debug.Log("玩家狗带了");
                 return;
             }
+            var myHeight = NpcObject.Position.y - CurrentNpcClimbHeight;
 
             var layer = obj.gameObject.layer;
+            if (obj.transform.tag.Equals("ForbiddenZone") && layer == LayerMask.NameToLayer("Ground"))
+            {
+                var c2d = obj.gameObject.GetComponent<BoxCollider2D>();
+                var targetHeight = obj.transform.position.y + c2d.bounds.size.y / 2;
+                if (myHeight > targetHeight)
+                {
+                    NpcObject.transform.position = new Vector3(NpcObject.Position.x, NpcObject.Position.y + GameDefine.StairsSlopeHeight, NpcObject.Position.z);
+                }
+            }
+
             if (layer == LayerMask.NameToLayer("Wall"))
             {
-                NpcObject.TurnRound();
+               NpcObject.TurnRound();
             }
         }
     }
