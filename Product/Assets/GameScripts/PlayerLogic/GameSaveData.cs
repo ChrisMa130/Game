33d29f4a5900d7854e445b44281f36e7da07b93e
jlu3389 <jlu3389@gmail.com>
2016-09-07 00:53:46 +0800

    提一波。还有save查入和预加载blank scene

--- Product/Assets/GameScripts/PlayerLogic/GameSaveData.cs
+++ Product/Assets/GameScripts/PlayerLogic/GameSaveData.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using SimpleJson;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MG
 {
@@ -27,15 +28,102 @@ namespace MG
             if (obj == null)
                 return false;
 
+            var cl = LoadLevelInfo(obj);
+            if (cl != null)
+            {
+                CurrentLevel = cl;
+            }
 
-            return false;
+            if (obj["PassLevel"] != null)
+            {
+                JsonArray pls = obj["PassLevel"] as JsonArray;
+                PassLevel = new List<LevelInfo>();
+
+                int count = pls.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    JsonObject pl = pls[i] as JsonObject;
+                    var l = LoadLevelInfo(pl);
+                    if (l != null)
+                    {
+                        PassLevel.Add(l);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private LevelInfo LoadLevelInfo(JsonObject obj)
+        {
+            LevelInfo info = null;
+
+            if (obj["CurrentLevel"] != null)
+            {
+                JsonObject cl = obj["CurrentLevel"] as JsonObject;
+                info = new LevelInfo();
+                info.Name = cl["Name"].ToString();
+
+                if (cl["CollectItem"] != null)
+                {
+                    JsonArray ci = cl["CollectItem"] as JsonArray;
+                    info.CollectItem = new List<int>();
+
+                    int len = ci.Count;
+                    for (int i = 0; i < len; i++)
+                    {
+                        int count = int.Parse(ci[i].ToString());
+                        info.CollectItem.Add(count);
+                    }
+                }
+            }
+
+            return info;
         }
 
         public bool Save()
         {
-            return false;
+            string t = SimpleJson.SimpleJson.SerializeObject(this);
+            string dataPath = Application.persistentDataPath + "\\save.dat";
+
+            File.WriteAllText(dataPath, t);
+
+            return true;
         }
 
+        public void SavePassLevelInfo()
+        {
+            if (PassLevel == null)
+                PassLevel = new List<LevelInfo>();
+
+            PassLevel.Add(CurrentLevel);
+            CurrentLevel = null;
+        }
+
+        public void SaveCurrentLevelInfo()
+        {
+            if (CurrentLevel == null)
+            {
+                CurrentLevel = new LevelInfo();               
+            }
+
+            var sn = SceneManager.GetActiveScene();
+            CurrentLevel.Name = sn.name;
+
+            if (CurrentLevel.CollectItem == null)
+            {
+                CurrentLevel.CollectItem = new List<int>();
+            }
+
+            CurrentLevel.CollectItem.Clear();
+
+            var player = GameMgr.Instance.PlayerLogic;
+            for (int i = 0; i < GameDefine.CollectCount; i++)
+            {
+                int count = player.GetCollectCount(i);
+                CurrentLevel.CollectItem.Add(i);
+            }
+        }
     }
 }
 
