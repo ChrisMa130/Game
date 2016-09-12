efbdb118049bb5ef8a3ed07206bde6599bc39982
ChrisMa <am.faylisa@Hotmail.com>
2016-09-12 00:52:19 -0700

    背景音乐控制器初步完成

--- Product/Assets/GameScripts/PlayerLogic/BGManager.cs
+++ Product/Assets/GameScripts/PlayerLogic/BGManager.cs
@@ -1,15 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+
 
 public class BGManager : MonoBehaviour {
+	private AudioSource currentMusic;
+	private AudioSource lastMusic;
+	private AudioSource[] sources;
 
+	public float FadeTime;
+	public float MaxVolume = 1.0f;
+	private float fadeSpeed;
 	// Use this for initialization
 	void Awake () {
-		DontDestroyOnLoad (transform.gameObject);
+		DontDestroyOnLoad(this.gameObject);
+		sources = GetComponents<AudioSource> ();
+		fadeSpeed = 1.0f / FadeTime;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (currentMusic != null) {
+			if (currentMusic.volume < MaxVolume)
+				fadeSequence (currentMusic, fadeSpeed);
+		}
+		if (currentMusic != sources [GetLevel ()]) {
+			lastMusic = currentMusic;
+			currentMusic = sources [GetLevel ()];
+			currentMusic.volume = 0f;
+			currentMusic.Play ();
+		}
+		if (lastMusic != null) {
+			if (lastMusic.volume > 0f)
+				fadeSequence (lastMusic, -fadeSpeed);
+			else
+				lastMusic.Stop ();
+		}
+	}
+
+	private void fadeSequence(AudioSource source,float fadeSpeed) {
+		source.volume += fadeSpeed * Time.deltaTime;
+	}
+
+	private int GetLevel() {
+		string sceneName = SceneManager.GetActiveScene ().name;
+		return int.Parse(sceneName.Split ("-"[0])[0])-1;
 	}
 }
