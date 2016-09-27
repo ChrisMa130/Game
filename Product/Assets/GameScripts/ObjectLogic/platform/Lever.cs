eb643217c9aff4c9ee93e780fd8868f5b3083ea7
jlu3389 <jlu3389@gmail.com>
2016-09-27 19:56:48 +0800

    拉杆动画BUG修复 #128

--- Product/Assets/GameScripts/ObjectLogic/platform/Lever.cs
+++ Product/Assets/GameScripts/ObjectLogic/platform/Lever.cs
@@ -1,95 +1,109 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Linq;
-namespace MG {
-	public class Lever : TimeUnit {
-		public PlatformBase[] Platforms;
-
+namespace MG
+{
+    public class Lever : TimeUnit
+    {
+        public PlatformBase[] Platforms;
 		public bool left = true;
-		private Animator anim;
-		private bool hasPlayer;
-		private Collider2D obj;
+        private Animator anim;
+        private bool hasPlayer;
+        private Collider2D obj;
 
-		class UserData : TimeUnitUserData
-		{
-			public bool hasPlayer;
-		}
+        class UserData : TimeUnitUserData
+        {
+            public bool hasPlayer;
+            public bool Left;
+        }
 
-		void Start () {
-			anim = transform.FindChild("lever").GetComponent<Animator> ();
+        void Start()
+        {
+            anim = transform.FindChild("lever").GetComponent<Animator>();
 			anim.SetBool ("Left", left);
-			hasPlayer = false;
-			Init(false);
-		}
+            hasPlayer = false;
+            Init(false);
+        }
 
-		void Update() {
-			if (Input.GetKeyUp ("w") && !anim.IsInTransition(0) && hasPlayer) {
-				anim.SetBool ("Left", !anim.GetBool ("Left"));
-				foreach (var p in Platforms.Where(p => p != null))
-				{
-					p.TurnOn(this.obj.gameObject);
-				}
-			}
-		}
+        void Update()
+        {
+            if (Input.GetKeyUp("w") && !anim.IsInTransition(0) && hasPlayer)
+            {
+                anim.SetBool("Left", !anim.GetBool("Left"));
+                foreach (var p in Platforms.Where(p => p != null))
+                {
+                    p.TurnOn(this.obj.gameObject);
+                }
+            }
+        }
 
-		void OnTriggerEnter2D (Collider2D obj) {
-			var npc = obj.GetComponent<Npc> ();
-			if (npc != null) {
-				if (!anim.IsInTransition(0)) {
-					anim.SetBool ("Left", !anim.GetBool ("Left"));
-					foreach (var p in Platforms.Where(p => p != null))
-					{
-						p.TurnOn(obj.gameObject);
-					}
-				}
-			}
-			var player = obj.GetComponent<Player>();
-			if (player != null) {
-				this.obj = obj;
-				hasPlayer = true;
-			}
-		}
+        void OnTriggerEnter2D(Collider2D obj)
+        {
+            var npc = obj.GetComponent<Npc>();
+            if (npc != null)
+            {
+                if (!anim.IsInTransition(0))
+                {
+                    anim.SetBool("Left", !anim.GetBool("Left"));
+                    foreach (var p in Platforms.Where(p => p != null))
+                    {
+                        p.TurnOn(obj.gameObject);
+                    }
+                }
+            }
+            var player = obj.GetComponent<Player>();
+            if (player != null)
+            {
+                this.obj = obj;
+                hasPlayer = true;
+            }
+        }
 
-		void OnTriggerExit2D (Collider2D obj) {
-			var player = obj.GetComponent<Player>();
-			if (player == null) {
-				return;
-			}
-			this.obj = null;
-			hasPlayer = false;
-		}
+        void OnTriggerExit2D(Collider2D obj)
+        {
+            var player = obj.GetComponent<Player>();
+            if (player == null)
+            {
+                return;
+            }
+            this.obj = null;
+            hasPlayer = false;
+        }
 
-		protected override TimeUnitUserData GetUserData()
-		{
-			var data = new UserData
-			{
-				hasPlayer = this.hasPlayer
-			};
+        protected override TimeUnitUserData GetUserData()
+        {
+            var data = new UserData
+            {
+                hasPlayer = this.hasPlayer,
+                Left = anim.GetBool("Left"),
+            };
 
-			return data;
-		}
+            return data;
+        }
 
-		protected override void SetUserData(TimeUnitUserData data)
-		{
-			UserData d = data as UserData;
-			if (d == null)
-				return;
+        protected override void SetUserData(TimeUnitUserData data)
+        {
+            UserData d = data as UserData;
+            if (d == null)
+                return;
 
-			this.hasPlayer = d.hasPlayer;
-		}
+            this.hasPlayer = d.hasPlayer;
 
-//		private void PlayAnimation() {
-//			anim.Play("TurnLeft", -1, 0f);
-//			if (Left) {
-//				anim ["TurnLeftLegacy"].speed = 0.7f;
-//				anim.Play ();
-//				Left = false;
-//			} else {
-//				anim ["TurnLeftLegacy"].speed = -0.7f;
-//				anim ["TurnLeftLegacy"].time = anim ["TurnLeftLegacy"].length;
-//				anim.Play ();
-//				Left = true;
-//			}
-//		}
-	}
+            anim.SetBool("Left", d.Left);
+        }
+
+        //		private void PlayAnimation() {
+        //			anim.Play("TurnLeft", -1, 0f);
+        //			if (Left) {
+        //				anim ["TurnLeftLegacy"].speed = 0.7f;
+        //				anim.Play ();
+        //				Left = false;
+        //			} else {
+        //				anim ["TurnLeftLegacy"].speed = -0.7f;
+        //				anim ["TurnLeftLegacy"].time = anim ["TurnLeftLegacy"].length;
+        //				anim.Play ();
+        //				Left = true;
+        //			}
+        //		}
+    }
 }
