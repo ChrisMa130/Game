e2674e3e3c620314c0aca9b6582cfe4c5b99e970
jlu3389 <jlu3389@gmail.com>
2016-11-09 17:15:56 +0800

    修改预设脚本，增加分析日志ID功能。

--- Product/Assets/GameScripts/ObjectLogic/Diaries.cs
+++ Product/Assets/GameScripts/ObjectLogic/Diaries.cs
@@ -1,20 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 namespace MG
 {
     public class Diaries : TimeUnit
     {
-        public int Id; // 日志ID
-
         void OnTriggerEnter2D(Collider2D other)
         {
+            string name = transform.parent.name;
+            int pos = name.LastIndexOf("_") + 1;
+            int id = Convert.ToInt32(name.Substring(pos));
+            Debug.Log("Id = " + id);
             if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
             {
                 var player = other.gameObject.GetComponent<Player>();
                 if (player != null)
                 {
-                    player.PickupDiarie(Id);
+                    player.PickupDiarie(id);
                     DestoryMe();
                 }
             }
