fe11849fddbf6ad7580fcc7229e49190824c6232
jlu3389 <jlu3389@gmail.com>
2016-11-18 21:44:42 +0800

    时间倒退的时候碰到刺的话然后再开始时间人不会死 #163

--- Product/Assets/GameScripts/ObjectLogic/Spike.cs
+++ Product/Assets/GameScripts/ObjectLogic/Spike.cs
@@ -4,19 +4,19 @@ namespace MG
 {
     public class Spike : MonoBehaviour
     {
-        void OnTriggerEnter2D(Collider2D other)
+        public void OnTriggerStay2D(Collider2D other)
         {
             if (TimeController.Instance.IsOpTime())
                 return;
 
             var player = other.GetComponent<Player>();
-            if (player != null)
+            if (player != null && !player.IsDead)
                 player.Dead();
 
-			//temp
-			var npc = other.GetComponent<Npc>();
-			if (npc != null)
-				npc.Dead();
+            //temp
+            var npc = other.GetComponent<Npc>();
+            if (npc != null && !npc.IsDead)
+                npc.Dead();
 
             var key = other.GetComponent<OpenKey>();
             if (key != null)
